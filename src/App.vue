efcd28bf7d5d2ef00e1628095505da7be056765c
BhryanS2 <bhryanstepenhen@gmail.com>
2022-08-16 19:40:42 -0300

    feat: 🛠   CrFazendo busca na API

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
-<div>
-  <h1>{{titulo}}</h1>
-  <img :src="foto.url" :alt="foto.descricao" :title="foto.titulo">
-  <h1>Lista</h1>
-  <ul>
-    <li v-for="foto in fotos" v-bind:key="foto.id">
-      <img :src="foto.url" :alt="foto.descricao" :title="foto.titulo">
-    </li>
-  </ul>
-
-</div>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto in fotos" v-bind:key="foto.url">
+        <img :src="foto.url" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -17,41 +14,16 @@ export default {
   data() {
     return {
       titulo: "Alurapic",
-      foto: {
-        titulo: "Foto Alura 1",
-        url: "https://picsum.photos/1200/600",
-        descricao: "Apenas uma forto aleatória"
-      },
-      fotos: [
-        {
-          id: 1,
-          titulo: "Foto Alura aleatória 1",
-          url: "https://picsum.photos/1200/600",
-          descricao: "Apenas uma forto aleatória"
-        },
-        {
-          id: 2,
-          titulo: "Foto Alura aleatória 2",
-          url: "https://picsum.photos/1200/600",
-          descricao: "Apenas uma forto aleatória"
-        },
-        {
-          id: 3,
-          titulo: "Foto Alura aleatória 3",
-          url: "https://picsum.photos/1200/600",
-          descricao: "Apenas uma forto aleatória"
-        },
-        {
-          id: 4,
-          titulo: "Foto Alura aleatória 4",
-          url: "https://picsum.photos/1200/600",
-          descricao: "Apenas uma forto aleatória"
-        }
-      ]
-    }
+      fotos: []
+    };
+  },
+  created() {
+    const promise = this.$http.get("http://localhost:3000/v1/fotos");
+    promise.then(response => {
+      this.fotos = response.data;
+    });
   }
-}
+};
 </script>
 
-<style>
-</style>
+<style></style>
