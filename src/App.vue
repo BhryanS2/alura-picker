0b2b3fbdf05d44e4421f1e02fa8c8deef4794cb4
BhryanS2 <bhryanstepenhen@gmail.com>
2022-08-16 19:26:06 -0300

    feat: 🛠   Criando lista dinamica

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
 <div>
   <h1>{{titulo}}</h1>
   <img :src="foto.url" :alt="foto.descricao" :title="foto.titulo">
-
+  <h1>Lista</h1>
+  <ul>
+    <li v-for="foto in fotos" v-bind:key="foto.id">
+      <img :src="foto.url" :alt="foto.descricao" :title="foto.titulo">
+    </li>
+  </ul>
 
 </div>
 </template>
@@ -13,10 +18,36 @@ export default {
     return {
       titulo: "Alurapic",
       foto: {
-        titulo: "Foto alura",
+        titulo: "Foto Alura 1",
         url: "https://picsum.photos/1200/600",
         descricao: "Apenas uma forto aleatória"
       },
+      fotos: [
+        {
+          id: 1,
+          titulo: "Foto Alura aleatória 1",
+          url: "https://picsum.photos/1200/600",
+          descricao: "Apenas uma forto aleatória"
+        },
+        {
+          id: 2,
+          titulo: "Foto Alura aleatória 2",
+          url: "https://picsum.photos/1200/600",
+          descricao: "Apenas uma forto aleatória"
+        },
+        {
+          id: 3,
+          titulo: "Foto Alura aleatória 3",
+          url: "https://picsum.photos/1200/600",
+          descricao: "Apenas uma forto aleatória"
+        },
+        {
+          id: 4,
+          titulo: "Foto Alura aleatória 4",
+          url: "https://picsum.photos/1200/600",
+          descricao: "Apenas uma forto aleatória"
+        }
+      ]
     }
   }
 }
